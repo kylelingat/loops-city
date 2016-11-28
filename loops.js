828c6728a428bb1ef931c4d0e0914125fa5a6d2d
Kyle Lingat <kylejlingat@gmail.com>
2016-11-28 08:35:30 -1000

    loops

---
+++ loops.js
@@ -0,0 +1,22 @@
+
+
+var count;
+
+for (count = 0; count <= 100; count++){
+  console.log(count);
+}
+
+
+
+var animalCount; 
+
+for (animalCount = 0; animalCount < 6; animalCount++) {
+  console.log(animalCount);
+  console.log('there are ' + animalCount + ' many animals in Noahs Arc');
+}
+
+var partyAnimals = ["lion", "dog", "cat", "bear", "tree"];
+
+for (var i in partyAnimals) {
+  console.log(partyAnimals[i])
+}
