49389f0abcb3564d30b075340276338ebc4f1444
kylelingat <kylejlingat@gmail.com>
2016-11-30 08:51:51 -1000

    Update loops.js

--- loops.js
+++ loops.js
@@ -16,9 +16,10 @@ for (animalCount = 0; animalCount < 6; animalCount++) {
 }
 
 var partyAnimals = ["lion", "dog", "cat", "bear", "tree"];
+var adj = ["green", "orange", "small", "grey", "ugly"]
 
 for (var i in partyAnimals) {
-  console.log(partyAnimals[i]);
+  console.log('the ' + partyAnimals[i] + ' is ' + adj[i]);
 }
 
 var weaponChest = ["Mace", "Sorcerer's Stone", "Excalibur"];
